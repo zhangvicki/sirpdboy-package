08cddfb620812f2f848526298a7264f6de10ac20
zhangvicki <70022965+zhangvicki@users.noreply.github.com>
2021-10-22 10:23:38 +0800

    Update eqos.lua

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -5,6 +5,5 @@ function index()
 		return
 	end
 
-    entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
     entry({"admin", "control", "eqos"}, cbi("eqos"), "网速限制", 90).dependent = true
 end
